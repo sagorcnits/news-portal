70f05ece36f60732fa8d6eb0aedc44eb9d3fbf98
sagorcnits <sagor.cnits@gmail.com>
2025-06-21 09:10:24 +0600

    login form added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-
+import LoginForm from "./_components/login-form";
 
 export default function Home() {
   return (
-    <div>
-        <h1 className="text-3xl font-bold mt-10">Welcome to Next.js!</h1>
-    </div>
+    <main >
+      <LoginForm />
+    </main>
   );
 }
